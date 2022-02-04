f99bf44b912dfa01fc20257f694448ab1eb62183
satorina-vDev <nonato.miau@hotmail.com>
2022-02-04 02:48:50 -0300

    trabalho2

---
+++ script.js
@@ -0,0 +1,19 @@
+let idade = 20
+let humano = true
+let aniversario = 5
+let janeiro = 4
+let dezembro = 6 
+if (idade >= 18 && humano == true) {
+    console.log("vc e maior de 18 anos ");
+}
+else{console.log("tchau")}
+
+if(janeiro < aniversario)
+{
+  console.log("você faz aniversario em janeiro");
+  (dezembro = dezembro - 5);
+}
+if(dezembro > aniversario)
+{
+  console.log("você faz aniversario em dezembroro");
+}
